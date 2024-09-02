aa1e743267f3c35adaae24cb0019dca4f529ab2f
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2024-09-02 22:31:49 +0200

    update

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,2 +1,44 @@
+use rayon::prelude::*;
+use std::sync::{Arc, Mutex};
+
 pub mod constant;
 pub mod simulation;
+
+pub fn simulate_n(
+    input_pulls: u32,
+    input_pity_character: usize,
+    input_capture_radiance: u32,
+    input_focus_character: bool,
+    input_pity_weapon: usize,
+    input_epitomized_path: bool,
+    input_focus_weapon: bool,
+    input_constellation: i32,
+    input_refinement: u32,
+    wanted_constellation: i32,
+    wanted_refinement: u32,
+) -> f64 {
+    let nb_simulation = 1_000_000;
+    let res = Arc::new(Mutex::new(0_f64));
+
+    (0..nb_simulation).into_par_iter().for_each(|_| {
+        let (pulls, _, _) = simulation::simulate(
+            input_pulls,
+            input_pity_character,
+            input_capture_radiance,
+            input_focus_character,
+            input_pity_weapon,
+            input_epitomized_path,
+            input_focus_weapon,
+            input_constellation,
+            input_refinement,
+            wanted_constellation,
+            wanted_refinement,
+        );
+        if pulls > 0 {
+            let mut res_lock = res.lock().unwrap();
+            *res_lock += 1.0;
+        }
+    });
+    let res = *res.lock().unwrap();
+    res / nb_simulation as f64
+}
--- src/index.rs
+++ src/index.rs
@@ -1,6 +1,10 @@
+use backend::simulate_n;
 use iced::{
     alignment,
-    widget::{button, column, container, row, scrollable, svg, text, text_input, Column},
+    widget::{
+        button, checkbox, column, container, row, scrollable, slider, svg, text, text_input,
+        Column, Space,
+    },
     Element, Length, Padding, Sandbox, Theme,
 };
 
@@ -16,6 +20,7 @@ pub struct Index {
     input_refinement: u32,
     wanted_constellation: i32,
     wanted_refinement: u32,
+    estimated_probability: f64,
 }
 
 impl Default for Index {
@@ -23,7 +28,7 @@ impl Default for Index {
         Self {
             input_pulls: 0,
             input_pity_character: 0,
-            input_capture_radiance: 0,
+            input_capture_radiance: 1,
             input_focus_character: false,
             input_pity_weapon: 0,
             input_epitomized_path: false,
@@ -32,17 +37,34 @@ impl Default for Index {
             input_refinement: 0,
             wanted_constellation: 0,
             wanted_refinement: 1,
+            estimated_probability: 0.0,
         }
     }
 }
 
 #[derive(Debug, Clone)]
-pub enum Amount {
-    InputValue(usize),
+pub enum Message {
+    PullsChanged(u32),
+    // Character
+    PityCharacterChanged(u32),
+    CaptureRadianceChanged(u32),
+    FocusCharacterChanged(bool),
+    // Weapon
+    PityWeaponChanged(u32),
+    EpitomizedPathChanged(bool),
+    FocusWeaponChanged(bool),
+    // Current
+    CurrentConstellationChanged(i32),
+    CurrentRefinementChanged(u32),
+    // Wanted
+    WantedConstellationChanged(i32),
+    WantedRefinementChanged(u32),
+    // Simulate
+    Simulate,
 }
 
 impl Sandbox for Index {
-    type Message = Amount;
+    type Message = Message;
 
     fn new() -> Self {
         Self::default()
@@ -52,15 +74,130 @@ impl Sandbox for Index {
         "index".into()
     }
 
-    fn update(&mut self, message: Self::Message) {}
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::PullsChanged(v) => self.input_pulls = v,
+            // Character
+            Message::PityCharacterChanged(v) => self.input_pity_character = v as usize,
+            Message::CaptureRadianceChanged(v) => self.input_capture_radiance = v,
+            Message::FocusCharacterChanged(v) => self.input_focus_character = v,
+            // Weapon
+            Message::PityWeaponChanged(v) => self.input_pity_weapon = v as usize,
+            Message::EpitomizedPathChanged(v) => self.input_epitomized_path = v,
+            Message::FocusWeaponChanged(v) => self.input_focus_weapon = v,
+            // Current
+            Message::CurrentConstellationChanged(v) => self.input_constellation = v,
+            Message::CurrentRefinementChanged(v) => self.input_refinement = v,
+            // Wanted
+            Message::WantedConstellationChanged(v) => self.wanted_constellation = v,
+            Message::WantedRefinementChanged(v) => self.wanted_refinement = v,
+            // Simulate
+            Message::Simulate => {
+                self.estimated_probability = simulate_n(
+                    self.input_pulls,
+                    self.input_pity_character,
+                    self.input_capture_radiance,
+                    self.input_focus_character,
+                    self.input_pity_weapon,
+                    self.input_epitomized_path,
+                    self.input_focus_weapon,
+                    self.input_constellation,
+                    self.input_refinement,
+                    self.wanted_constellation,
+                    self.wanted_refinement,
+                );
+            }
+        }
+    }
 
     fn view(&self) -> Element<Self::Message> {
-        container(text(""))
-            .height(Length::Fill)
-            .width(Length::Fill)
-            .align_x(alignment::Horizontal::Center)
-            .align_y(alignment::Vertical::Center)
-            .into()
+        container(column!(
+            row!(
+                text("nb pulls: "),
+                slider(0..=1000, self.input_pulls, Message::PullsChanged),
+                text(format!(" Current value: {}", self.input_pulls)),
+            ),
+            column!(
+                text("\n=== CHARACTER ==="),
+                text(""),
+                row!(
+                    text("pity character: "),
+                    slider(
+                        0..=89,
+                        self.input_pity_character as u32,
+                        Message::PityCharacterChanged
+                    ),
+                    text(format!(" Current value: {}", self.input_pity_character)),
+                ),
+                row!(
+                    text("capture radiance: "),
+                    slider(
+                        1..=4,
+                        self.input_capture_radiance,
+                        Message::CaptureRadianceChanged
+                    ),
+                    text(format!(" Current value: {}", self.input_capture_radiance)),
+                ),
+                row!(checkbox("garanteed character", self.input_focus_character)
+                    .on_toggle(Message::FocusCharacterChanged)),
+                text("\n=== WEAPON ==="),
+                text(""),
+                row!(
+                    text("pity weapon: "),
+                    slider(
+                        0..=76,
+                        self.input_pity_weapon as u32,
+                        Message::PityWeaponChanged
+                    ),
+                    text(format!(" Current value: {}", self.input_pity_weapon)),
+                ),
+                row!(checkbox("epitomized path", self.input_epitomized_path)
+                    .on_toggle(Message::EpitomizedPathChanged)),
+                row!(checkbox("garanteed focus weapon", self.input_focus_weapon)
+                    .on_toggle(Message::FocusWeaponChanged)),
+            ),
+            column!(
+                // vertical line
+            ),
+            column!(
+                text("Current constellation"),
+                slider(
+                    -1..=6,
+                    self.input_constellation,
+                    Message::CurrentConstellationChanged
+                ),
+                text("Current refinement"),
+                slider(
+                    0..=5,
+                    self.input_refinement,
+                    Message::CurrentRefinementChanged
+                ),
+                text("Wanted constellation"),
+                slider(
+                    -1..=6,
+                    self.wanted_constellation,
+                    Message::WantedConstellationChanged
+                ),
+                text("Wanted refinement"),
+                slider(
+                    0..=5,
+                    self.wanted_refinement,
+                    Message::WantedRefinementChanged
+                ),
+            ),
+            row!(
+                button("Submit").on_press(Message::Simulate),
+                text(format!(
+                    "Estimated probability: {}",
+                    self.estimated_probability
+                ))
+            )
+        ))
+        .height(Length::Fill)
+        .width(Length::Fill)
+        .align_x(alignment::Horizontal::Center)
+        .align_y(alignment::Vertical::Center)
+        .into()
     }
 
     fn theme(&self) -> Theme {
